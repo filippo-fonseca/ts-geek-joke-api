830bc7b6dc2b27763c0d303b86c6f4b7ff8ab185
filippo-fonseca <filifonsecacagnazzo@gmail.com>
2022-05-21 00:30:57 -0600

    export

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import axios from "axios";
  * Incredibly complex. This is a next level function that returns a nerdy quote.
  * @return A random quote from the Geek Quote API.
  */
-async function getQuote() {
+export async function getQuote() {
   try {
     const response = await axios.get("https://geek-jokes.sameerkumar.website/api");
     return response.data;
